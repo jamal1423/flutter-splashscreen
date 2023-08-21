8110b9f67eb2d75ebf4e9562330c768e436a24ff
Mochammad Jamal <moch.jamal87@gmail.com>
2023-08-21 20:56:13 +0700

    splashscreen flutter

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+// import 'package:flame_splash_screen/flame_splash_screen.dart';
+import 'package:splashscreen/page_home.dart';
+// import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Main Page'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 10),
+            ()=>Navigator.pushReplacement(context,
+            MaterialPageRoute(builder:
+                (context) =>
+                    const PageHome()
+            )
+        )
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.white,
+        // child:FlutterLogo(size:MediaQuery.of(context).size.height)
+        child: const Image(
+          image: AssetImage("assets/loading.gif"),
+          width: 100,
+          height: 80,
+        ),
+    );
+  }
+}
---
+++ lib/page_home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PageHome extends StatefulWidget {
+  const PageHome({super.key});
+
+  @override
+  State<PageHome> createState() => _PageHomeState();
+}
+
+class _PageHomeState extends State<PageHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 236, 236, 236),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 203, 243, 230),
+        title: const Text("Homepage"),
+      ),
+    );
+  }
+}
